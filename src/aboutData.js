87c9f9d914916a816acf6f44d9b54eefdb6e2ea8
Rishav Raj <iamrishavraj1@gmail.com>
2022-12-19 19:50:36 +0530

    Update aboutData.js

--- src/aboutData.js
+++ src/aboutData.js
@@ -7,7 +7,7 @@ const about = [
     brandLogo: "./assets/lg.svg",
     describe: "A few words about me",
     resume:
-      "https://drive.google.com/file/d/1pkjEFDQo4EqLGtYSDIc5jgcXqt9S-Diy/view",
+      "https://drive.google.com/file/d/1bEZiZEIqsGYwnV9R2uGJ4uR3pU9ZJlv9/view",
     detail: `I'm a developer and final-year mechanical engineering student who discovered his true interest in programming. I've always been fascinated by how computers and mobile applications respond to commands. During my first semester, I learnt about programming and began teaching myself how to code. After a few weeks, I realised that this is what I wanted to pursue with my life, and I fell in love with the beauty of programming. It requires a blend of creative and logical problem-solving abilities, and there is always something new to learn.`,
   },
 ];
