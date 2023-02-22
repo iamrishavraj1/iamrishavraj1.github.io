c4497a50f369351a84f182786a16c9f90253a028
Rishav Raj <iamrishavraj1@gmail.com>
2023-02-22 12:33:15 +0530

    Remove G4

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import ReactGA from "react-ga4";
+// import ReactGA from "react-ga4";
 const Form = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
